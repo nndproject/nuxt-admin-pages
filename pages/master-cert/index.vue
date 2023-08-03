948200a5075091421bf476c6e13bfba573539a3b
nndproject <teamdevnnd@gmail.com>
2023-08-03 17:10:15 +0700

    Update index.vue

--- pages/master-cert/index.vue
+++ pages/master-cert/index.vue
@@ -1,3 +1,4 @@
+<!-- https://tairo.cssninja.io/dashboards/personal-2 -->
 <template>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
@@ -45,6 +46,123 @@
           </div>
         </div>
       </div> -->
+      <div
+        class="border-muted-200 dark:border-muted-700 dark:bg-muted-800 relative w-full border bg-white transition-all duration-300 rounded-md p-5"
+      >
+        <!--[-->
+        <div class="flex flex-col items-center md:flex-row">
+          <div
+            class="ltablet:flex-row ltablet:items-center flex flex-col items-center gap-4 text-center md:items-start md:text-left lg:flex-row lg:items-center"
+          >
+            <div
+              class="relative inline-flex shrink-0 items-center justify-center outline-none h-20 w-20 rounded-full"
+            >
+              <div
+                class="rounded-full flex h-full w-full items-center justify-center overflow-hidden text-center transition-all duration-300"
+              >
+                <!--[--><img
+                  src="/img/avatars/2.svg"
+                  class="h-20 w-20 max-h-full max-w-full object-cover shadow-sm dark:border-transparent"
+                /><!----><!----><!--]-->
+              </div>
+              <div
+                class="h-8 w-8 bottom-0 end-0 dark:bg-muted-800 absolute z-10 block overflow-hidden rounded-full bg-white"
+              >
+                <!--[--><img
+                  src="/img/icons/flags/united-states-of-america.svg"
+                  class="relative h-full w-full scale-90 rounded-full object-cover"
+                  alt=""
+                /><!--]-->
+              </div>
+              <!---->
+            </div>
+            <div class="text-center md:text-left">
+              <h2
+                class="font-heading text-xl font-light leading-tight text-muted-800 dark:text-white"
+              >
+                <!--[--><span>Welcome back, Maya</span
+                ><!--]-->
+              </h2>
+              <p
+                class="font-alt text-base font-normal leading-normal leading-normal"
+              >
+                <!--[--><span class="text-muted-400"
+                  >It's nice to see you again</span
+                ><!--]-->
+              </p>
+            </div>
+          </div>
+          <div
+            class="ltablet:flex-row ltablet:items-center ms-auto flex flex-col gap-6 text-center md:text-left lg:flex-row lg:items-center"
+          >
+            <div class="flex-1">
+              <h3
+                class="font-heading text-3xl font-semibold leading-tight text-muted-800 dark:text-white"
+              >
+                <!--[--><span>
+                  31 <small class="text-base font-medium">Tasks</small></span
+                ><!--]-->
+              </h3>
+              <p
+                class="font-alt text-base font-normal leading-normal leading-normal"
+              >
+                <!--[--><span class="text-muted-400 text-sm">
+                  Are currently pending </span
+                ><!--]-->
+              </p>
+            </div>
+            <div
+              class="border-muted-200 dark:border-muted-700 dark:bg-muted-800 relative w-full border bg-white transition-all duration-300 rounded-md from-primary-600 to-primary-700 shadow-primary-500/20 relative flex flex-1 items-center justify-center bg-gradient-to-br p-5 shadow-xl"
+            >
+              <!--[-->
+              <div class="relative z-20 flex flex-col gap-3">
+                <p
+                  class="font-alt text-sm font-normal leading-normal leading-normal"
+                >
+                  <!--[--><span class="text-white/80">
+                    Start using our team and project management tools </span
+                  ><!--]-->
+                </p>
+                <a
+                  aria-current="page"
+                  href="/dashboards/personal-2#"
+                  class="router-link-active router-link-exact-active font-sans text-sm text-white underline-offset-4 hover:underline"
+                >
+                  Learn More
+                </a>
+              </div>
+              <div
+                class="absolute bottom-0 end-2 z-10 flex h-14 w-14 items-center justify-center"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  aria-hidden="true"
+                  role="img"
+                  class="icon text-primary-900/50 h-10 w-10"
+                  style=""
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 256 256"
+                  data-v-cd102a71=""
+                >
+                  <g fill="currentColor">
+                    <path
+                      d="m239.78 86.62l-37 113.38S176 184 128 184s-74.78 16-74.78 16l-37-113.37a4.1 4.1 0 0 1 5.72-5l53.41 26.62a4.11 4.11 0 0 0 5.36-1.56L124.48 34a4.11 4.11 0 0 1 7 0l43.77 72.74a4.12 4.12 0 0 0 5.35 1.56l53.43-26.64a4.1 4.1 0 0 1 5.75 4.96Z"
+                      opacity=".2"
+                    ></path>
+                    <path
+                      d="M243.84 76.19a12.08 12.08 0 0 0-13.34-1.7l-50.21 25l-41.92-69.63a12.11 12.11 0 0 0-20.74 0L75.71 99.52l-50.19-25a12.11 12.11 0 0 0-16.9 14.6l37 113.36a8 8 0 0 0 11.68 4.4c.25-.15 25.82-14.88 70.7-14.88s70.45 14.73 70.68 14.87a8 8 0 0 0 11.71-4.39l37-113.33a12.06 12.06 0 0 0-3.55-12.96ZM198 188.83c-12-5.09-35.92-12.83-70-12.83s-58 7.74-70 12.83L26.71 93l45.07 22.47a12.17 12.17 0 0 0 15.78-4.59L128 43.66l40.44 67.2a12.18 12.18 0 0 0 15.77 4.59L229.29 93Zm-22.13-32a8 8 0 0 1-7.87 6.61a8.36 8.36 0 0 1-1.4-.12a228.2 228.2 0 0 0-77.22 0a8 8 0 0 1-2.78-15.76a244.42 244.42 0 0 1 82.78 0a8 8 0 0 1 6.5 9.24Z"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+              <!--]-->
+            </div>
+          </div>
+        </div>
+        <!--]-->
+      </div>
     </div>
   </main>
 </template>
